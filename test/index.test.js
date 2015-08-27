9e882a9691eec951c8f8f143f95e0a73d66d352c
Ryan Clark <ryan.clark.j@gmail.com>
2015-08-27 09:57:30 -0700

    meow meow cc @willwhite

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,10 @@
 var test = require('tape');
 var cat = require('..');
 
-test('starts with c', function(assert) {
-  var str = cat();
-  assert.equal(str[0], 'c', 'starts with c');
-  assert.end();
-});
+for (var i = 0; i < 10000; i++) {
+  test('starts with c', function(assert) {
+    var str = cat();
+    assert.equal(str[0], 'c', 'starts with c');
+    assert.end();
+  });
+}
